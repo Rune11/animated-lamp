708c011a4e2ada4bda579a15b8d058dd6bf49d0e
Gergely Fábián <gergoo2@gmail.com>
2019-01-05 15:24:20 +0100

    Add empty inventory class

---
+++ XamarinRpg/XamarinRpg/Model/Inventory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinRpg.Model
+{
+    public class Inventory
+    {
+        #region private data
+
+        #endregion region
+
+        #region Contructor
+        public Inventory() {
+
+        }
+        #endregion
+    }
+}
