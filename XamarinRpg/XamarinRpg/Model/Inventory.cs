2ac07dfaaa77c0a5d25c88bd0399575140b01bba
Gergely Fábián <gergoo2@gmail.com>
2019-01-05 15:30:20 +0100

    Add ItemList and capacity

--- XamarinRpg/XamarinRpg/Model/Inventory.cs
+++ XamarinRpg/XamarinRpg/Model/Inventory.cs
@@ -7,12 +7,14 @@ namespace XamarinRpg.Model
     public class Inventory
     {
         #region private data
-
+        private List<Item> ItemList;
+        private int Capacity;
         #endregion region
 
         #region Contructor
         public Inventory() {
-
+            ItemList = new List<Item>();
+            Capacity = 12;
         }
         #endregion
     }
