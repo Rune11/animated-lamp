0b9d9ce78ecac0c28c1120fa0d2b7b000cf47649
Gergely Fábián <gergoo2@gmail.com>
2019-01-05 15:25:45 +0100

    Add empty Item class

---
+++ XamarinRpg/XamarinRpg/Model/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinRpg.Model
+{
+    class Item
+    {
+        #region private data
+
+        #endregion
+
+        #region Constructor
+        public Item() {
+
+        }
+        #endregion
+    }
+}
