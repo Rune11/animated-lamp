7f757c4a57d9f3a2f9c1a28dbfa7dfe89b59600c
Gergely Fábián <gergoo2@gmail.com>
2019-01-06 12:57:05 +0100

    Use properties instead of fields

--- XamarinRpg/XamarinRpg/Model/Character.cs
+++ XamarinRpg/XamarinRpg/Model/Character.cs
@@ -7,46 +7,40 @@ namespace XamarinRpg.Model
 
     public class Character
     {
-        #region private data
 
-        private string Name;
+        #region Properties
 
-        private int Strength;
+        public string Name { get; set; }
+        public int Str { get; set; }
+        public int Agi { get; set; }
+        public int End { get; set; }
+        public int Intel { get; set; }
+        public int Wis { get; set; }
+        public int MaxHP { get; set; }
+        public int CurrHP { get; set; }
+        public int Lvl { get; set; }
+        public int Xp { get; set; }
+        public Inventory Inv { get; set; }
 
-        private int Agility;
-
-        private int Endurance;
-
-        private int Intelligence;
-
-        private int Wisdom;
-
-        private int MaxHP;
-
-        private int CurrentHP;
-
-        private int Level;
-
-        private int XP;
-
-        private Inventory Inv;
         #endregion
 
         #region Constructor
         // Contructor for new character
-        public Character(string name) {
-            Name = name;
-            Strength = 0;
-            Agility = 0;
-            Endurance = 0;
-            Intelligence = 1;
-            Wisdom = 0;
+        public Character(string charName) {
+            Name = charName;
+            Str = 0;
+            Agi = 0;
+            End = 0;
+            Intel = 1;
+            Wis = 0;
             MaxHP = 100;
-            CurrentHP = MaxHP;
-            Level = 1;
-            XP = 0;
+            CurrHP = MaxHP;
+            Lvl = 1;
+            Xp = 0;
             Inv = new Inventory();
         }
+
+        
 
         // Constructor for loading an existing character
         // most likely we wont need that constructror
